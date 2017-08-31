a2df5de80a006adace1b240ec2464a4017fb2104
Don Gourley <gourley@islandora.wrlc.org>
2017-08-31 14:15:03 -0400

    production client app ID and redirect URI; resolves #1

--- config.php
+++ config.php
@@ -13,10 +13,10 @@ $scope_desc['/read-limited'] = "Read your limited-access information";
 $scope_desc['/activities/update'] = "Add or update your research activities";
 $scope_desc['/person/update'] = "Add or update your personal information";
 
-#$home = "https://dra.american.edu/orcid/";
-$home = "https://api-stage.wrlc.org/orcid/";
-#$info = "https://orcid.org";
-$info = "https://sandbox.orcid.org";
+$home = "https://dra.american.edu/orcid/";
+#$home = "https://api-stage.wrlc.org/orcid/";
+$info = "https://orcid.org";
+#$info = "https://sandbox.orcid.org";
 $audra= "http://dra.american.edu/audra-ir";
 
 $project1pager = "https://docs.google.com/document/d/1HygRQ6hqoElILQvGjxkgZ4cSS_Y4B4vbx59Ex0ApFB0/edit?usp=sharing";
@@ -24,10 +24,12 @@ $project1pager = "https://docs.google.com/document/d/1HygRQ6hqoElILQvGjxkgZ4cSS_
 
 // ORCID API CREDENTIALS
 ////////////////////////////////////////////////////////////////////////
-
-define('OAUTH_CLIENT_ID', 'APP-010CTGKA36MQQR7X');
-define('OAUTH_REDIRECT_URI', 'https://api-stage.wrlc.org/orcid/oauth-redirect.php');
-#define('OAUTH_REDIRECT_URI', 'https://dra.american.edu/orcid/oauth-redirect.php');
+// ORCID sandbox API app:
+#define('OAUTH_CLIENT_ID', 'APP-010CTGKA36MQQR7X');
+#define('OAUTH_REDIRECT_URI', 'https://api-stage.wrlc.org/orcid/oauth-redirect.php');
+// ORCID production API app:
+define('OAUTH_CLIENT_ID', 'APP-B9KGUIQ1W750ISKG');
+define('OAUTH_REDIRECT_URI', 'https://dra.american.edu/orcid/oauth-redirect.php');
 // define OAUTH_CLIENT_SECRET in this file:
 require_once('oauth-client-secret.php');
 
@@ -35,16 +37,16 @@ require_once('oauth-client-secret.php');
 ////////////////////////////////////////////////////////////////////////
 
 // Sandbox - Member API
-define('OAUTH_AUTHORIZATION_URL', 'https://sandbox.orcid.org/oauth/authorize');//authorization endpoint
-define('OAUTH_TOKEN_URL', 'https://sandbox.orcid.org/oauth/token'); //token endpoint
+//define('OAUTH_AUTHORIZATION_URL', 'https://sandbox.orcid.org/oauth/authorize');//authorization endpoint
+//define('OAUTH_TOKEN_URL', 'https://sandbox.orcid.org/oauth/token'); //token endpoint
 
 // Sandbox - Public API
 //define('OAUTH_AUTHORIZATION_URL', 'https://sandbox.orcid.org/oauth/authorize');//authorization endpoint
 //define('OAUTH_TOKEN_URL', 'https://pub.sandbox.orcid.org/oauth/token');//token endpoint
 
 // Production - Member API
-//define('OAUTH_AUTHORIZATION_URL', 'https://orcid.org/oauth/authorize');//authorization endpoint
-//define('OAUTH_TOKEN_URL', 'https://orcid.org/oauth/token'); //token endpoint
+define('OAUTH_AUTHORIZATION_URL', 'https://orcid.org/oauth/authorize');//authorization endpoint
+define('OAUTH_TOKEN_URL', 'https://orcid.org/oauth/token'); //token endpoint
 
 // Production - Public API
 //define('OAUTH_AUTHORIZATION_URL', 'https://orcid.org/oauth/authorize');//authorization endpoint
